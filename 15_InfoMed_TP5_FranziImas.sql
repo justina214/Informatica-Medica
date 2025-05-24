01addf41cec55026aaccff86c790865b3144db73
justina214 <120291001+justina214@users.noreply.github.com>
2025-05-24 15:01:27 +0200

    Create 15_InfoMed_TP5_FranziImas.sql
    Agrego Ejercicio 15

---
+++ 15_InfoMed_TP5_FranziImas.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.nombre AS nombre_paciente,
+    c.fecha AS FechaUltimaConsulta,
+	c.diagnostico AS Diagnostico
+FROM Pacientes p
+LEFT JOIN Consultas c ON p.id_paciente = c.id_paciente
+WHERE c.fecha = (SELECT MAX(fecha)
+	FROM Consultas
+	WHERE id_paciente=p.id_paciente);
